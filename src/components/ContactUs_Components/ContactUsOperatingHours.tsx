a14607a11b65325d6a8b47633d2d8ef09e74ce45
RamtinPB <ramtin.panahibazaz@gmail.com>
2024-12-28 14:08:34 +0330

    responsive header

--- src/components/ContactUs_Components/ContactUsOperatingHours.tsx
+++ src/components/ContactUs_Components/ContactUsOperatingHours.tsx
@@ -105,7 +105,7 @@ export default function ContactUsOperationHours() {
       <section className="bg-gray-100 pt-52">
         <div className="container mx-auto px-5">
           <div className="flex- mb-10 flex-col items-center justify-center text-center">
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="green"
@@ -116,7 +116,7 @@ export default function ContactUsOperationHours() {
                 d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
                 clip-rule="evenodd"
               />
-            </svg>
+            </svg> */}
             {/* @ts-ignore */}
             <Typography variant="h3" color="black">
               Our Operating Hours
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,22 +3,33 @@ import {
   Typography,
   Button,
   IconButton,
-  MobileNav,
+  List,
+  ListItem,
 } from "@material-tailwind/react";
+import { SetStateAction, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const [selected, setSelected] = useState(1);
+  const setSelectedItem = (value: SetStateAction<number>) => setSelected(value);
+
+  const [showMenu, setShowMenu] = useState(false);
+  function clickMenuBar() {
+    setShowMenu((prev) => !prev);
+  }
+
   const navigate = useNavigate();
 
   return (
     <>
       {/* @ts-ignore */}
       <Navbar className="sticky top-0 z-50 mx-auto h-max max-w-full rounded-none border-none bg-gradient-to-b from-gray-100 to-transparent px-4 py-2 shadow-lg lg:px-8 lg:py-4">
-        <div className="container mx-auto flex items-center justify-between gap-5">
-          <div className="flex items-center justify-center gap-1">
-            <div className="flex gap-2">
-              {/* <img src="" alt="KNC Logo" className="h-11 w-11" /> */}
-              {/* <div className="grid h-11 w-11 place-items-center rounded-full bg-gray-300">
+        <div className="container mx-auto flex flex-col items-center justify-between">
+          <div className="gap- container flex items-center justify-between gap-5">
+            <div className="flex items-center justify-center gap-1">
+              <div className="flex gap-2">
+                {/* <img src="" alt="KNC Logo" className="h-11 w-11" /> */}
+                {/* <div className="grid h-11 w-11 place-items-center rounded-full bg-gray-300">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -34,83 +45,89 @@ export default function Header() {
                   />
                 </svg>
               </div> */}
-              {/* @ts-ignore */}
-              <Typography
-                variant="h4"
-                className="mx-4 py-1.5 font-body text-green-600"
-              >
-                Kian Nursing Center
-              </Typography>
+                {/* @ts-ignore */}
+                <Typography
+                  variant="h4"
+                  className="text-md mx-4 !text-nowrap py-1.5 font-body text-green-600 md:text-2xl"
+                >
+                  Kian Nursing Center
+                </Typography>
+              </div>
+
+              <div className="hidden h-8 border-l border-gray-500 px-1.5 md:block" />
+
+              <div className="hidden md:block">
+                {/* @ts-ignore */}
+                <List className="flex items-center gap-6">
+                  {/* @ts-ignore */}
+                  <ListItem>
+                    {/* @ts-ignore */}
+                    <Typography
+                      onClick={() => {
+                        navigate("/");
+                        window.scrollTo(0, 0);
+                      }}
+                      variant="small"
+                      color="blue-gray"
+                      className="cursor-pointer p-1 font-medium hover:text-green-600"
+                    >
+                      Home
+                    </Typography>
+                  </ListItem>
+                  {/* @ts-ignore */}
+                  <ListItem>
+                    {/* @ts-ignore */}
+                    <Typography
+                      onClick={() => {
+                        navigate("/AboutUs");
+                        window.scrollTo(0, 0);
+                      }}
+                      variant="small"
+                      color="blue-gray"
+                      className="cursor-pointer p-1 font-medium hover:text-green-600"
+                    >
+                      About Us
+                    </Typography>
+                  </ListItem>
+                  {/* @ts-ignore */}
+                  <ListItem>
+                    {/* @ts-ignore */}
+                    <Typography
+                      onClick={() => {
+                        navigate("/Services");
+                        window.scrollTo(0, 0);
+                      }}
+                      variant="small"
+                      color="blue-gray"
+                      className="cursor-pointer p-1 font-medium hover:text-green-600"
+                    >
+                      Services
+                    </Typography>
+                  </ListItem>
+                  {/* @ts-ignore */}
+                  <ListItem>
+                    {/* @ts-ignore */}
+                    <Typography
+                      onClick={() => {
+                        navigate("/ContactUs");
+                        window.scrollTo(0, 0);
+                      }}
+                      variant="small"
+                      color="blue-gray"
+                      className="cursor-pointer p-1 font-medium hover:text-green-600"
+                    >
+                      Contact Us
+                    </Typography>
+                  </ListItem>
+                </List>
+              </div>
             </div>
 
-            <div className="h-8 border-l border-gray-500 px-1.5" />
-
-            <div className="hidden lg:block">
-              <ul className="flex items-center gap-6">
-                <li>
-                  {/* @ts-ignore */}
-                  <Typography
-                    onClick={() => {
-                      navigate("/");
-                      window.scrollTo(0, 0);
-                    }}
-                    variant="small"
-                    color="blue-gray"
-                    className="cursor-pointer p-1 font-medium hover:text-green-600"
-                  >
-                    Home
-                  </Typography>
-                </li>
-                <li>
-                  {/* @ts-ignore */}
-                  <Typography
-                    onClick={() => {
-                      navigate("/AboutUs");
-                      window.scrollTo(0, 0);
-                    }}
-                    variant="small"
-                    color="blue-gray"
-                    className="cursor-pointer p-1 font-medium hover:text-green-600"
-                  >
-                    About Us
-                  </Typography>
-                </li>
-                <li>
-                  {/* @ts-ignore */}
-                  <Typography
-                    onClick={() => {
-                      navigate("/Services");
-                      window.scrollTo(0, 0);
-                    }}
-                    variant="small"
-                    color="blue-gray"
-                    className="cursor-pointer p-1 font-medium hover:text-green-600"
-                  >
-                    Services
-                  </Typography>
-                </li>
-                <li>
-                  {/* @ts-ignore */}
-                  <Typography
-                    onClick={() => {
-                      navigate("/ContactUs");
-                      window.scrollTo(0, 0);
-                    }}
-                    variant="small"
-                    color="blue-gray"
-                    className="cursor-pointer p-1 font-medium hover:text-green-600"
-                  >
-                    Contact Us
-                  </Typography>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div>
             {/* @ts-ignore */}
             <Button
-              className="bg-black transition-colors duration-300 hover:bg-green-600"
+              ripple={true}
+              color="black"
+              className="hidden transition-colors duration-300 hover:bg-green-600 md:block"
               onClick={() => {
                 navigate("/ContactUs");
                 window.scrollTo(0, 0);
@@ -118,28 +135,137 @@ export default function Header() {
             >
               Book an Appointment
             </Button>
-          </div>
 
-          {/* @ts-ignore */}
-          <MobileNav className="lg:hidden">
             {/* @ts-ignore */}
-            <IconButton variant="text">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
+            <IconButton
+              className="bg-inherit shadow-none md:hidden"
+              onClick={() => clickMenuBar()}
+            >
+              <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+                {showMenu ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="black"
+                    aria-hidden="true"
+                    data-slot="icon"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 18 18 6M6 6l12 12"
+                    ></path>
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="black"
+                    aria-hidden="true"
+                    data-slot="icon"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    ></path>
+                  </svg>
+                )}
+              </span>
             </IconButton>
-          </MobileNav>
+          </div>
+          <div
+            className={`container w-full overflow-hidden transition-[max-height] duration-500 ease-in-out md:hidden ${showMenu ? "max-h-[1000px]" : "max-h-0"}`}
+          >
+            {/* @ts-ignore */}
+            <List className="flex flex-col items-start">
+              {/* @ts-ignore */}
+              <ListItem
+                selected={selected === 1}
+                onClick={() => setSelectedItem(1)}
+              >
+                {/* @ts-ignore */}
+                <Typography
+                  onClick={() => {
+                    navigate("/");
+                    window.scrollTo(0, 0);
+                  }}
+                  variant="small"
+                  color="blue-gray"
+                  className="cursor-pointer p-1 font-medium hover:text-green-600"
+                >
+                  Home
+                </Typography>
+              </ListItem>
+              {/* @ts-ignore */}
+              <ListItem>
+                {/* @ts-ignore */}
+                <Typography
+                  onClick={() => {
+                    navigate("/AboutUs");
+                    window.scrollTo(0, 0);
+                  }}
+                  variant="small"
+                  color="blue-gray"
+                  className="cursor-pointer p-1 font-medium hover:text-green-600"
+                >
+                  About Us
+                </Typography>
+              </ListItem>
+              {/* @ts-ignore */}
+              <ListItem>
+                {/* @ts-ignore */}
+                <Typography
+                  onClick={() => {
+                    navigate("/Services");
+                    window.scrollTo(0, 0);
+                  }}
+                  variant="small"
+                  color="blue-gray"
+                  className="cursor-pointer p-1 font-medium hover:text-green-600"
+                >
+                  Services
+                </Typography>
+              </ListItem>
+              {/* @ts-ignore */}
+              <ListItem>
+                {/* @ts-ignore */}
+                <Typography
+                  onClick={() => {
+                    navigate("/ContactUs");
+                    window.scrollTo(0, 0);
+                  }}
+                  variant="small"
+                  color="blue-gray"
+                  className="cursor-pointer p-1 font-medium hover:text-green-600"
+                >
+                  Contact Us
+                </Typography>
+              </ListItem>
+              {/* @ts-ignore */}
+              <ListItem>
+                {/* @ts-ignore */}
+                <Button
+                  ripple={true}
+                  size="sm"
+                  color="black"
+                  className="transition-colors duration-300 hover:bg-green-600"
+                  onClick={() => {
+                    navigate("/ContactUs");
+                    window.scrollTo(0, 0);
+                  }}
+                >
+                  Book an Appointment
+                </Button>
+              </ListItem>
+            </List>
+          </div>
         </div>
       </Navbar>
     </>
